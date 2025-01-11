adbd378ffcecd1bd4c74e51c9a826e4d7ae992c7
Code with Cigar <sinharoysaptarshi30@gmail.com>
2025-01-11 23:06:06 +0530

    Initial commit

---
+++ day_5.js
@@ -0,0 +1,33 @@
+//! = date
+// ** what i learn 
+
+
+const prompt = require("prompt-sync")();
+// take input from user
+let number = prompt("Enter a nutarual number ");
+let format = parseInt(number);
+let sum = 0;
+
+for (let i = 0; i < format; i++) {
+  sum += (i + 1);
+}
+console.log(sum);
+
+
+// !today 11-1-25 
+// ** i learn for in loops in morning
+// i learn for in loops 
+
+
+letgranny = {
+  harry: 56,
+  tarry: 56,
+  yarry: 56,
+  tarry: 56,
+  larry: 56
+};
+
+for (let a in granny) {
+  console.log("marks of " + a + " is " +granny[a])
+    
+  }
