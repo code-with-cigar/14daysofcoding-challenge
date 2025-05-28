96e226a92b60d0109e8436c24c632e2d5cbd356a
Code with Cigar <sinharoysaptarshi30@gmail.com>
2025-05-28 22:26:54 +0530

    day 1
    learn and practice

---
+++ every_day_source_code/day_1.js
@@ -0,0 +1,40 @@
+// objects in js
+
+// const obj = {
+//   name: "Alice",
+//   age: 30,
+//   isStudent: false,
+// };
+
+// console.log(obj["isStudent"]);
+
+//funtion in js
+
+// function avg(x, y, z) {
+//   return (x + y + z) / 3;
+// }
+
+// console.log(avg(4, 5, 6));
+
+// if else in js
+/*
+let age = 10;
+
+if (age > 18) {
+  console.log("You can drive car.");
+} else {
+  console.log("You cannot drive car.");
+}
+*/
+
+// practice of funtion and if else
+
+// function coke(x) {
+//   if (x >= 18) {
+//     console.log("you can drink coke");
+//   } else {
+//     console.log("you cannot drink coke");
+//   }
+// }
+
+// coke(13);
