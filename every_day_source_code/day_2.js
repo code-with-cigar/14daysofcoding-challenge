c53163519e3c8ef10cae8e6ffa2819df3a321810
Code with Cigar <sinharoysaptarshi30@gmail.com>
2025-05-29 22:45:49 +0530

    day 2 of #14daysofcoding
    practice some loops

---
+++ every_day_source_code/day_2.js
@@ -0,0 +1,16 @@
+// practice some loops 
+
+
+
+let t = [1,2,3,4,5,6,7,8];
+
+for (let i = 0;i < t.length; i++){
+    console.log(t[i]);
+}
+
+
+let c = 0;
+while (c < t.length) {
+  console.log(t[c]);
+  c++;
+}
